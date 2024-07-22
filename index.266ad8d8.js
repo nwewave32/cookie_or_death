ded167023fd1a4001fa16c3c8ab1f8fc41ade2dc
nwewave32 <nwewave.32@gmail.com>
2024-07-23 01:38:45 +0900

    Updates

--- index.266ad8d8.js
+++ index.266ad8d8.js
@@ -1,2 +1,2 @@
 document.querySelector("#content").addEventListener("click",function(e){location.href="lang.html"});
-//# sourceMappingURL=index.08a09ddd.js.map
+//# sourceMappingURL=index.266ad8d8.js.map
